4a5e81df0f32fa0a17dd335f134b7b9924322130
LukasOy <lukas.oylu@gmail.com>
2022-11-25 00:08:28 +0000

    listacoordinada

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,55 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [tarea, nueva] = useState([]);
+		
+	
+	useEffect(() => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/lulukkks")
+		.then((response) => response.json())
+		.then((data) => nueva(data));
+		}, []);
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		
+			<div className="text-center">
+
+				<h1 >Lista de Tareas </h1>
+
+				<form  onSubmit={(event)=>{
+					event.preventDefault();
+					nueva([...tarea,event.target[0].value]);
+				}}>
+
+				<input placeholder="¿cual es tu tarea?"></input>
+				<button> Ingresar</button>
+				</form >
+				{tarea.map((value,index)=>{
+					return <li key={index}>{value.label} 
+					<button> X </button></li>
+			
+				})}
+				<h6>Cantidad de tareas</h6>
+			</div>
 	);
 };
 
 export default Home;
+
+
+/*useEffect(() => {
+    fetch("http://assets.breatheco.de/apis/fake/todos/user/lulukkks")
+    .then((response) => response.json())
+    .then((data) => setLista(data.results));
+    }, [];
+    return (
+        <di v>
+
+        </div>
+    )
+
+*/
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 //import react into the bundle
-import React from "react";
+
 import ReactDOM from "react-dom";
+import React, {useState, useEffect} from "react";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
